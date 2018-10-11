e2ec009e4823877b34f9c1f2d73a727cfb4a4fa2
emanueleParlascino <G00362655@gmit.ie>
2018-10-11 14:57:07 +0100

    Initial check-in of module simple

---
+++ simple/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace simple
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeLabel_Clicked(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Hello World " +
+                              entryUsername.Text +
+                              " !!!!";
+            entryUsername.Text = "";
+            btnChangeLabel.Text = "Dont do that again";
+        }
+    }
+}
